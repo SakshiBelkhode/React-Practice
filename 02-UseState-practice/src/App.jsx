5caecaa19a5e4b3a372219ec0d325671b9b2b281
SakshiBelkhode <126399972+SakshiBelkhode@users.noreply.github.com>
2026-02-02 18:18:12 +0530

    Practiced  Questions of Useeffect

--- 02-UseState-practice/src/App.jsx
+++ 02-UseState-practice/src/App.jsx
@@ -1,19 +1,35 @@
-/*Counter Practice*/
+/*Counter Practice
 
-import { useState } from 'react'
-function App () {
-  const [count, setCount] = useState(0);
+import { useState } from "react";
 
+function App (){
+  const [count , setCount] = useState(0);
+ 
 return(
   <>
-    <h1>{count}</h1>
-    <button onClick={() => setCount(count+1)}>+</button>
-  
+   <h1>{count}</h1> 
+   <button onClick = {() => setCount (Count + 1)}>+</button> 
   
   </>
-);
-}
+)  
 
-export default App
+}
+export default App*/ 
+
+/*Input Box*/ 
+
+import { useState } from "react";
+
+function App() {
+  const [name, setName] = useState("");
+
+  return (
+    <>
+      <input onChange={e => setName(e.target.value)} />
+      <h1>{name}</h1>
+    </>
+  );
+}
+export default App;
 
 
