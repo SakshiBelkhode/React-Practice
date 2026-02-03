a9338626d52dd544644a32b515093bdb20d150bc
SakshiBelkhode <126399972+SakshiBelkhode@users.noreply.github.com>
2026-02-03 17:26:19 +0530

    Practiced Done with Five Different questions of react Usestate

--- 02-UseState-practice/src/App.jsx
+++ 02-UseState-practice/src/App.jsx
@@ -1,22 +1,39 @@
-/*Counter Practice
+/*Counter Practice*/
 
-import { useState } from "react";
+import { useState } from 'react'
 
 function App (){
   const [count , setCount] = useState(0);
+  const [name, setName] = useState("");  /*Input Box*/
+  const [show, setShow] = useState(true); /*Show Hide text*/
+  const [color, setColor] = useState("red"); /*Change Color*/
+  const [liked, setLiked] = useState(false); /*Like Button*/
+
  
 return(
   <>
    <h1>{count}</h1> 
-   <button onClick = {() => setCount (Count + 1)}>+</button> 
-  
+   <button onClick = {() => setCount (count + 1)}>+</button> 
+
+   <input onChange={e => setName(e.target.value)} />
+   <h1>{name}</h1>
+
+   <button onClick={ () => setShow(!show)}Toggle></button>
+   {show && <p>Hello</p>}
+
+   <h1 style = {{color}}>Color</h1>
+   <button onClick= {() => setColor("blue")}>Change</button>
+   
+   <button onClick={() => setLiked(!liked)}>
+      {liked ? "Liked ❤️" : "Like 🤍"}
+    </button>
   </>
 )  
 
 }
-export default App*/ 
+export default App
 
-/*Input Box*/ 
+/*Input Box
 
 import { useState } from "react";
 
@@ -30,6 +47,22 @@ function App() {
     </>
   );
 }
-export default App;
+export default App;*/
+
+
+/*Show-Hide Text
+
+import { useState } from "react";
+
+function App() {
+  const [show, setShow] = useState(true);
+return (
+  <>
+    <button onClick={ () => setShow(!show)}Toggle></button>
+    {show && <p>Hello</p>}
+  </>
+);
+}
+ export default App;*/
 
 
