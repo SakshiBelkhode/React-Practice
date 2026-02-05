37c892cd7aff8669c932d0e9c5b7d48d8ca66fc0
SakshiBelkhode <126399972+SakshiBelkhode@users.noreply.github.com>
2026-02-05 20:20:37 +0530

    Practiced Done with Axios-API

---
+++ 05-AxiosAPI-practice/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState, useEffect } from 'react'
+
+function App() {
+  const[users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(res => res.json())
+    .then(data => setUsers(data));
+  },[]);
+
+  return(
+    <ul>
+      {
+        users.map(user => (
+          <li key = {user.id}>{user.name}</li>
+        ))
+      }
+    </ul>
+  );
+}
+
+export default App
