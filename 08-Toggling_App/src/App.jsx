cc4789ba0069174ae9fd08e06f51fbee94b52484
SakshiBelkhode <126399972+SakshiBelkhode@users.noreply.github.com>
2026-02-25 16:40:31 +0530

    Learned the concept of Toggle App

---
+++ 08-Toggling_App/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+function App() {
+
+  // Step 1: Create state
+  const [status, setStatus] = useState(false);
+
+  // Step 2: Function to change state
+  function handleClick() {
+    setStatus(!status);
+  }
+
+  return (
+    <div className="container">
+
+      <h1>Simple Toggle App</h1>
+
+      {/* Step 3: Show ON or OFF */}
+      <h2>{status ? "ON" : "OFF"}</h2>
+
+      {/* Step 4: Button */}
+      <button onClick={handleClick}>
+        Toggle
+      </button>
+
+    </div>
+  );
+}
+
+export default App;
