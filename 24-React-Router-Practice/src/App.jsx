5c88b86cf64077dd73def89ca438820a4878bac0
SakshiBelkhode <126399972+SakshiBelkhode@users.noreply.github.com>
2026-03-10 17:15:52 +0530

    Practiced React router by creating practice project

--- 24-React-Router-Practice/src/App.jsx
+++ 24-React-Router-Practice/src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route, Link } from "react-router-dom";
-import Home from "./Pages/Home.jsx";
-import About from "./Pages/About.jsx";
-import Contact from "./Pages/Contact.jsx";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 const App = () => {
   return (
